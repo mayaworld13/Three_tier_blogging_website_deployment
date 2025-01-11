fd870e5c18f9b46cce097d1e686a34265af8c101
Mayank sharma <mayank14082003@gmail.com>
2025-01-11 13:48:55 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -12,7 +12,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $USER
 newgrp docker
 sudo apt install docker-compose -y
 
